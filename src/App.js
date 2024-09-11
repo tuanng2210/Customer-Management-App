0b6ab0de6736f64e25099a96dfd67ce52d2a433b
tuanng2210 <103005131+tuanng2210@users.noreply.github.com>
2024-09-10 21:57:14 -0400

    delete unecessary import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Table, Card, Form, Button, Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import { getAll, deleteById, post, put } from "./utility/api";
 import CustomerList from "./components/CustomerList";
 import CustomerAddUpdateForm from "./components/CustomerAddUpdateForm";
